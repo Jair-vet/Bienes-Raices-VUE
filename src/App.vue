fe4d1ae286e169753648c253b0583ab0a61ee2e0
Jair <jairaceves56@gmail.com>
2023-11-24 18:21:10 -0600

    Implementamos el Routing

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,43 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <v-card
+    elevation="3"
+    max-width="1200"
+    class="mx-auto"
+  >
+    <v-layout>
+      <v-app-bar
+        color="cyan-darken-3"
+      >
+        <template v-slot:prepend>
+          <!-- Home -->
+          <v-btn
+            :to="{name: 'home'}"
+          >
+            Bienes Raices - VueFire
+          </v-btn>
+        </template>
+        <!-- Login -->
+        <template v-slot:append >
+          <v-btn
+            :to="{name: 'home'}"
+          >
+            Inicio
+          </v-btn>
+          <v-btn
+            :to="{name: 'login'}"
+          >
+            Iniciar Sesión
+          </v-btn>
+        </template>
+      </v-app-bar>
 
-  <RouterView />
+      <v-main>
+        <v-container>
+          <RouterView />
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
-
-<style scoped>
-
-</style>
